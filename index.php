a50229b36081eb8f5fe197578dd96a3760769952
Alexander <alexander_krupnin@mail.ru>
2022-12-12 22:33:21 +0300

    3

--- index.php
+++ index.php
@@ -1,109 +1,41 @@
 <?php
-
-    //--------------------------Настройки подключения к БД-----------------------
     include "db.php";
-    //----------------------------------------------------------------------------------------
-    $user_id = 1;
-    include "count_id.php";
-
-//создание временной таблиццы, где только устройства определененого человека
-$mysql->query("DROP TABLE IF EXISTS user_device;");
-$mysql->query("CREATE TEMPORARY TABLE user_device as SELECT * from DEVICE_TABLE as d JOIN USER_TABLE as u ON d.USER_ID = u.ID WHERE d.USER_ID = '$user_id'");
-$mysql->query("ALTER table user_device add id_i int primary key auto_increment;");
-
-
-echo'
-    <!DOCTYPE HTML>
-    <html>
-    <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>MyApp</title>
-    <script src="UpdateScript.js"> </script>
-    <script src="update_counter.js"> </script>
-    <link rel="manifest" href = "site.webmanifest">
-    <link rel="stylesheet" href = "index.css">
-    </head>
-    <body>';
-    
-    for($i = 1; $i <= $count_id; $i++){
-        $result = mysqli_query($mysql, "SELECT ud.DEVICE_ID from user_device as ud WHERE ud.id_i = '$i';");
-        $Arr = mysqli_fetch_array($result);
-        $id = $Arr['DEVICE_ID'];
-
-        /*if(empty($count_click_server)){
-            $count_click_server = 0;
-        }
-        echo'Кол-во кликов от сервера: '.$count_click_server.'';*/
-        
-        include "sql.php";
-        echo'
-        <p >'.$i.'</p>
-        ';
-        $date_minuta = date("Y-m-d H:i");
-        $result = mysqli_query($mysql, "SELECT count(*) as coun from HYSTORY as h where h.DATE_TIME LIKE('$date_minuta%') and h.DEVICE_ID = '$i';");
-        $Arr = mysqli_fetch_array($result);
-        $count_click_user = $Arr['coun'];
-        /*
-        if($count_click_server >= 10 * $count_id){
-            Echo'Обнаружена множестванная атака запросов на сервер.<br>
-            В качестве защиты вам будет ограничен доступ к данным на время';
-            break;
-        }
-        */
-        if($count_click_user >= 5){
-            echo'Вы много раз обращались к этому устройству.<br>
-            Рекомендация: отдых от 1 минуты<br>';
-        }else{
-        echo' <div class = "div_main">
-        <table class = "table_main">
-        <tr>
-        <td width=100px> Устройство:
-        </td>
-        <td width=238px>'.$device_name.'
-        </td>
-        </tr>
-        </table class = "table_main">
-        <table border=1 class = "table_main">
-        <tr>
-        <td width=100px> Tемпературавыа
-        </td>
-        <td width=40px>'.$temperature.'
-        </td>
-        <td width=150px>'.$temperature_dt.'
-        </td>
-        </tr>
-        <tr>
-        <td width=100px > Реле
-        </td>
-        <td width=40px>'.$out_state.'
-        </td>
-        <td width=150px>'.$out_state_dt.'
-        </td>
-        </tr>
-        </table><br>
-        ';
-        if($out_state == 0){
-            echo'<form method=POST > 
-            <button class = "button_main" formmethod=POST name=button_on'.$id.' value=1>Включить реле</button>
-            </form><br>';
-        }else{
-            echo'<form method=POST>
-            <button class = "button_main" formmethod=POST name=button_off'.$id.' value=1>Выключить реле</button>
-            </form><br>';
-        }
-        echo'
-        <form method=POST>
-    <button class = "button_main" formmethod=POST name=button_pr'.$id.' value=1>История</button></div>
-    </form><br>
-    </div >';
-        /*
-        Текущая минута: '.$date_minuta.'<br>
-        Кол-во кликов от юзера: '.$count_click_user.'<br>
-        Кол-во кликов от сервера: '.$count_click_server.'
-        */
-        }
-    }
-echo'
-    </body>
-    </html>';
-
-    ?>
+echo '
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>My App</title>
+      <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+      <link rel="manifest" href="site.webmanifest">
+    <link rel="stylesheet" href="index.css">
+  </head>
+  <body>
+    <header>
+    </header>';
+ echo'
+    <div class= "div_history">
+    <form action="http://lazy:3006/index.php" method="post">
+    <p>Логин: <input type="text" name="name" /></p>
+    <p>Пароль: <input type="text" name="password" /></p>
+    <p><input class = "button_main"  type="submit" /></p>
+   </form>
+    </div>';
+    echo'
+  </body>
+  </html>';
+  ?>
+ <?php
+ $name = $_POST['name'];
+ $password = $_POST['password'];
+ echo '<div>' . $name . '</div>';
+ echo '<div>' . $password . '</div>';
+ $io = 'ID';
+ $result = mysqli_query($mysql, "SELECT '$io' FROM `USER_TABLE` WHERE USER_NAME ='$name' AND PASSWORD = '$password'");
+ if (mysqli_num_rows($result) != "") { //Если в БД есть данные о температуре для этого устройства
+    echo '<div>' . mysqli_num_rows($result) . '</div>';
+     $id_user = mysqli_num_rows($result);
+     echo '<div>' . $id_user . '</div>';
+     Header("Location:main.php?id=$id_user");
+ }
+ ?>
